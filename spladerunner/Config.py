3ea853c3478ed473a3d3dba662d408f0b792ebf5
Prithivida <d.prithiviraj@gmail.com>
2024-04-25 11:45:39 +0700

    Update Config.py

--- spladerunner/Config.py
+++ spladerunner/Config.py
@@ -1,4 +1,4 @@
-MODEL_URL = 'https://storage.googleapis.com/flash-rank/{}.zip'
+MODEL_URL = 'https://huggingface.co/prithivida/flashrank/resolve/main/{}.zip'
 DEFAULT_CACHE_DIR = "/tmp"
 DEFAULT_MODEL = "Splade_PP_en_v1"
 MODEL_FILE_MAP = {
